313278cd6944f6e81bd4617789f23345c637fda6
Yaowei Deng <deng.wei285@gmail.com>
2020-07-03 17:33:57 -0700

    add playlist script

--- library_analysis.r
+++ library_analysis.r
@@ -23,6 +23,11 @@ get_avg <- function(x) {
   return(x)
 }
 
+get_200_avg <- function(x) {
+  x <- stats::filter(x, rep(1/200, 200), sides = 1)
+  return(x)
+}
+
 # import .csv file --------------------------------------------------------
 
 tracks_all <- get_file("song_library.csv")
@@ -41,25 +46,45 @@ tracks <- tracks[order(tracks$date_added),]
 tracks_met <- tracks %>%
   select(-'tempo', -'instrumentalness', -'loudness', -'speechiness', 
          -'time_signature')
+tracks_200_avg <- tracks_met
 tracks_met <- data.frame(tracks_met[1:6], apply(tracks_met[7:12], 2, get_avg))
+tracks_200_avg <- data.frame(tracks_200_avg[1:6], apply(tracks_200_avg[7:12], 2, get_200_avg))
+tracks_200_avg <- tracks_200_avg %>%
+  drop_na()
 
 # visualizations ----------------------------------------------------------
 
 metrics <- c('popularity', 'acousticness', 'danceability', 'energy', 'liveness', 'valence')
 colors <- c('firebrick', 'aquamarine2', 'skyblue', 'green4', 'gold', 'violet')
 title <- 'average metric values over time of Spotify song library'
+tracks_met <- tail(tracks_met, -10)
 tracks_met <- gather(tracks_met, key = metric, value = measure, metrics)
+tracks_200_avg <- gather(tracks_200_avg, key = metric, value = measure, metrics)
 plot1 <- ggplot(tracks_met, aes(x = date_added, y = measure, group = metric, color = metric)) + 
   geom_line() +
   scale_color_manual(values = colors) +
   ylab('metric') + 
-  xlab('time') +
+  xlab('date') +
   ggtitle(title)
-  plot2 <- ggplot(tracks_met, aes(x = date_added, y = measure, group = metric, color = metric)) + 
+plot2 <- ggplot(tracks_met, aes(x = date_added, y = measure, group = metric, color = metric)) + 
   geom_line() +
   scale_color_manual(values = colors) +
   facet_wrap(~ metric, scales = "free") + 
-  xlab('time')
+  xlab('date')
   ggtitle(title)
+plot3 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
+  geom_line() +
+  scale_color_manual(values = colors) +
+  ylab('metric') + 
+  xlab('date') +
+  ggtitle('rolling 200-song average metric values of Spotify library over time')
+plot4 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
+  geom_line() +
+  scale_color_manual(values = colors) +
+  facet_wrap(~ metric, scales = "free") + 
+  xlab('date') +
+  ggtitle('rolling 200-song average metric values of Spotify library over time')
 plot1
 plot2
+plot3
+plot4
