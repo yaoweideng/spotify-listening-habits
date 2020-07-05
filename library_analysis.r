89d2159f40194f0319b5e16ba5c53ddef0e20e34
Yaowei Deng <deng.wei285@gmail.com>
2020-07-05 04:12:38 -0700

    added genres + viz

--- library_analysis.r
+++ library_analysis.r
@@ -3,8 +3,10 @@
 
 library(dplyr)
 library(tidyr)
+library(reshape2)
 library(ggplot2)
 library(viridis)
+library(RColorBrewer)
 source("directory.r")
 
 # function definitions ----------------------------------------------------
@@ -29,11 +31,28 @@ get_200_avg <- function(x) {
   return(x)
 }
 
+get_avg_genre <- function(x, y) {
+  n <- nrow(x) / 2
+  for(i in 1:length(y)){
+    name <- y[i]
+    x[,name] <- x$genre
+    for(j in 1:nrow(x)) {
+      if(x[,name][j] == y[i]){
+        x[,name][j] = 1
+      } else {
+        x[,name][j] = 0
+      }
+    }
+    x[,name] <- stats::filter(x[,name], rep(1/n, n), sides = 1)
+  }
+  return(x)
+}
+ 
 # import .csv file --------------------------------------------------------
 
 tracks_all <- get_file("song_library.csv")
 
-# processing --------------------------------------------------------------
+# processing pt.1 --------------------------------------------------------------
 
 tracks <- tracks_all %>%
   select(-'X') %>%
@@ -46,55 +65,105 @@ tracks$date_added <- as.POSIXct(tracks$date_added, tz = "UTC", "%Y-%m-%dT%H:%M:%
 tracks <- tracks[order(tracks$date_added),]
 tracks_met <- tracks %>%
   select(-'tempo', -'instrumentalness', -'loudness', -'speechiness', 
-         -'time_signature')
+         -'time_signature', -'artist_genre1', -'artist_genre2', -'artist_popularity')
 tracks_200_avg <- tracks_met
 tracks_met <- data.frame(tracks_met[1:6], apply(tracks_met[7:12], 2, get_avg))
+
+# processing pt. 2 --------------------------------------------------------
+
 tracks_200_avg <- data.frame(tracks_200_avg[1:6], apply(tracks_200_avg[7:12], 2, get_200_avg))
 tracks_200_avg <- tracks_200_avg %>%
   drop_na()
-
-# visualizations pt. 1 ----------------------------------------------------------
-
 metrics <- c('popularity', 'acousticness', 'danceability', 'energy', 'liveness', 'valence')
 colors <- c('firebrick', 'aquamarine2', 'skyblue', 'green4', 'gold', 'violet')
-title <- 'average metric values over time of Spotify song library'
 tracks_met <- tail(tracks_met, -10)
 tracks_met <- gather(tracks_met, key = metric, value = measure, metrics)
 tracks_200_avg <- gather(tracks_200_avg, key = metric, value = measure, metrics)
+
+# processing pt. 3 --------------------------------------------------------
+
+dens <- c('valence', 'danceability')
+tracks_dens <- gather(tracks, key = metric, value = measure, dens)
+tracks_genre <- tracks %>%
+  select('date_added', 'artist_genre1', 'artist_genre2') 
+tracks_genre <- melt(tracks_genre, id.var = 'date_added', variable.name = 'genre')
+tracks_genre <- tracks_genre %>%
+  select(-'genre')
+colnames(tracks_genre) <- c('date_added', 'genre')
+tracks_genre <- tracks_genre[which(tracks_genre$genre != ""),]
+most_pop_genres <- tracks_genre %>% 
+  group_by(genre) %>% 
+  summarize(n = n(), .groups = 'drop') %>% 
+  top_n(12)
+most_pop_genres2 <- most_pop_genres %>%
+  top_n(8)
+genre_names <- as.character(most_pop_genres2$genre)
+genres_avg <- get_avg_genre(tracks_genre, genre_names) %>%
+  drop_na()
+genres_avg <- gather(genres_avg, key = genre, value = measure, genre_names)
+c_pop_indices <- which(tracks$artist_genre1 == 'c-pop')
+
+# visualizations pt. 1 ----------------------------------------------------------
+
 plot1 <- ggplot(tracks_met, aes(x = date_added, y = measure, group = metric, color = metric)) + 
   geom_line() +
   scale_color_manual(values = colors) +
   ylab('metric') + 
-  xlab('date') +
-  ggtitle(title)
+  ggtitle('average metric values over time of Spotify song library') +
+  theme(axis.title.x = element_blank())
+
 plot2 <- ggplot(tracks_met, aes(x = date_added, y = measure, group = metric, color = metric)) + 
   geom_line() +
   scale_color_manual(values = colors) +
   facet_wrap(~ metric, scales = "free") + 
-  xlab('date')
-  ggtitle(title)
-plot3 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
-  geom_line() +
-  scale_color_manual(values = colors) +
-  ylab('metric') + 
-  xlab('date') +
-  ggtitle('rolling 200-song average metric values of Spotify library over time')
-plot4 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
-  geom_line() +
-  scale_color_manual(values = colors) +
-  facet_wrap(~ metric, scales = "free") + 
-  xlab('date')
+  theme(axis.title.x = element_blank())
 
 # visualizations pt. 2 ----------------------------------------------------
 
-dens <- c('valence', 'danceability')
-tracks_dens <- gather(tracks, key = metric, value = measure, dens)
+plot3 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
+  geom_line() +
+  scale_color_manual(values = colors) +
+  ylab('measure') + 
+  ggtitle('rolling 200-song average metric values of Spotify library over time') +
+  theme(axis.title.x = element_blank())
+
+plot4 <- ggplot(tracks_200_avg, aes(x = date_added, y = measure, group = metric, color = metric)) + 
+  geom_line() +
+  scale_color_manual(values = colors) +
+  facet_wrap(~ metric, scales = "free") +
+  theme(axis.title.x = element_blank())
+
+# visualizations pt. 3 ----------------------------------------------------
+
 plot5 <- ggplot(tracks_dens, aes(x = measure, color = metric, fill = metric)) +
   geom_histogram(aes(y = ..density..), binwidth = 0.01, position = 'identity', alpha = 0.4) +
   geom_density(alpha = 0.6) +
   scale_color_manual(values = c("black", "black")) +
   scale_fill_manual(values = c("seagreen2", "skyblue")) +
   ggtitle("comparing densities between song danceability and song valence in library")
+
+plot6 <- ggplot(most_pop_genres, aes(x = genre, y = n, group = genre, fill = genre)) +
+  geom_bar(stat = 'identity') +
+  scale_fill_viridis(discrete = TRUE) +
+  ggtitle('most popular genres across song library (top 12 genres)') +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.y = element_blank())
+
+plot7 <- ggplot(genres_avg, aes(x = date_added, y = measure, group = genre, color = genre)) + 
+  geom_line() +
+  scale_color_brewer(palette = 'Set3') +
+  ylab('percentage of Spotify library') + 
+  ggtitle('average genre composition of Spotify library over time (top 8 genres)') +
+  theme(axis.title.x = element_blank())
+
+plot8 <- ggplot(genres_avg, aes(x = date_added, y = measure, group = genre, color = genre)) + 
+  geom_line() +
+  scale_color_brewer(palette = 'Set3') +
+  facet_wrap(~ genre, scales = "free") + 
+  ylab('percentage of Spotify library') +
+  theme(axis.title.x = element_blank())
 
 # see plots ---------------------------------------------------------------
 
@@ -103,3 +172,6 @@ plot2
 plot3
 plot4
 plot5
+plot6
+plot7
+plot8
--- song_library.py
+++ song_library.py
@@ -20,37 +20,34 @@ if token:
 NaN = np.nan
 
 #%% function definitions
-def next_tracks(results, array):
-    for i, item in enumerate(results['items']):
-        track = item['track']
-        array.append(track['id'])
-
-def next_dates(results, array):
-    for i, item in enumerate(results['items']):
-        date = item['added_at']
-        array.append(date)
-        
-def get_track_ids():
+def next_tracks(results, array, option):
+    if (option == 1):
+        for i, item in enumerate(results['items']):
+             track = item['track']
+             array.append(track['id'])
+    elif (option == 2):
+        for i, item in enumerate(results['items']):
+             date = item['added_at']
+             array.append(date)
+    else: 
+        for i, item in enumerate(results['items']):
+             track = item['track']
+             artist = track['artists'][0]
+             array.append(artist['id'])
+                
+def get_ids(option):
     ids = []
     tracks = sp.current_user_saved_tracks()
-    next_tracks(tracks, ids)
+    next_tracks(tracks, ids, option)
     while tracks['next']:
         tracks = sp.next(tracks)
-        next_tracks(tracks, ids)
-    return ids    
+        next_tracks(tracks, ids, option)
+    return ids
 
-def get_add_dates():
-    dates = []
-    tracks = sp.current_user_saved_tracks()
-    next_dates(tracks, dates)
-    while tracks['next']:
-        tracks = sp.next(tracks)
-        next_dates(tracks, dates)
-    return dates
-
-def get_track_info(id, date):
+def get_track_info(id, date, artist):
     info = sp.track(id)
     feats = sp.audio_features(id)
+    artists = sp.artist(artist)
     date_added = date
 
     name = info['name']
@@ -59,8 +56,18 @@ def get_track_info(id, date):
     release_date = info['album']['release_date']
     length = info['duration_ms']
     popularity = info['popularity']
+    artist_popularity = artists['popularity']
+    
+    if len(artists['genres']) == 0:
+        artist_genre1 = NaN
+    else:
+        artist_genre1 = artists['genres'][0]
+    if len(artists['genres']) >= 2:
+        artist_genre2 = artists['genres'][1]
+    else:
+        artist_genre2 = NaN
   
-    if(feats[0] == None):
+    if (feats[0] == None):
         feats[0] = {'acousticness' : NaN,
                     'danceability' : NaN,
                     'energy' : NaN,
@@ -83,27 +90,30 @@ def get_track_info(id, date):
     time_signature = feats[0]['time_signature']
     valence = feats[0]['valence']
 
-    track = [name, album, artist, release_date, date_added, length, popularity, 
-             acousticness, danceability, energy, instrumentalness, 
-             liveness, loudness, speechiness, tempo, time_signature, valence]
+    track = [name, album, artist, artist_genre1, artist_genre2, release_date, 
+             date_added, length, popularity, artist_popularity, acousticness, 
+             danceability, energy, instrumentalness, liveness, loudness, 
+             speechiness, tempo, time_signature, valence]
     return track
 
 #%% getting track ids and dates added
-ids = get_track_ids()
-dates = get_add_dates()
+ids = get_ids(1)
+dates = get_ids(2)
+artists = get_ids(3)
 
 #%% getting all the track info into a dataframe
 tracks = []
 
 for i in range(len(ids)):
-    track = get_track_info(ids[i], dates[i])
+    track = get_track_info(ids[i], dates[i], artists[i])
     tracks.append(track)
 
 df = pd.DataFrame(tracks, columns = 
-                  ['name', 'album', 'artist', 'release_date', 'date_added', 
-                   'length', 'popularity', 'acousticness', 'danceability',
-                   'energy', 'instrumentalness', 'liveness', 'loudness',
-                   'speechiness', 'tempo', 'time_signature', 'valence'])
+                  ['name', 'album', 'artist', 'artist_genre1', 'artist_genre2', 
+                   'release_date', 'date_added', 'length', 'popularity', 
+                   'artist_popularity', 'acousticness', 'danceability', 'energy', 
+                   'instrumentalness', 'liveness', 'loudness', 'speechiness', 
+                   'tempo', 'time_signature', 'valence'])
 
 #%% exporting dataframe into a .csv file
 df.to_csv("song_library.csv", sep = '|')
