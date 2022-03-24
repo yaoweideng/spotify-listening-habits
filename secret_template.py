b36716f4b498ac3c3be7282ddb15f490f6eba85a
Yaowei Deng <deng.wei285@gmail.com>
2022-03-23 22:11:09 -0700

    delete extraneous files

--- secret_template.py
+++
@@ -1,7 +0,0 @@
-client_id = 'your-client-id-here'
-client_secret = 'your-client-secret-here'
-username = 'your-Spotify-username-here'
-redirect_uri = 'your-redirect-uri-here'
-
-# if you are working with a playlist instead of liked songs
-playlist_id = 'your-playlist-uri-here'
